445fb9e06c9c3fc9fcb63e885b486e3d67eaa371
andresrojassshere <andres.camilo.rojas@outlook.com>
2023-11-26 19:48:31 -0500

    Corrección preguntas

--- src/website/pages/QuizPage.jsx
+++ src/website/pages/QuizPage.jsx
@@ -8,7 +8,6 @@ export function QuizPage() {
   const [sliderValue3, setSliderValue3] = useState(1);
   const [sliderValue4, setSliderValue4] = useState(1);
   const [sliderValue5, setSliderValue5] = useState(1);
-  const [sliderValue6, setSliderValue6] = useState(1);
 
   const handleSliderChange = (value) => {
     setSliderValue(value);
@@ -30,13 +29,6 @@ export function QuizPage() {
     setSliderValue5(value);
   };
 
-  const handleSliderChange6 = (value) => {
-    setSliderValue6(value);
-  };
-
-
-
-
   //Mandar info al back-end
   const enviarAlQuiz = async () => {
     const datosUsuario = {
@@ -45,29 +37,28 @@ export function QuizPage() {
       categoria3: sliderValue3,
       categoria4: sliderValue4,
       categoria5: sliderValue5,
-      categoria6: sliderValue6,
     };
 
     try {
-      const respuesta = await fetch('http://localhost:5000/calcular_similitud', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ datos_usuario: datosUsuario }),
-      });
+      const respuesta = await fetch(
+        "http://localhost:5000/calcular_similitud",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ datos_usuario: datosUsuario }),
+        }
+      );
 
       const datos = await respuesta.json();
-      console.log('Resultados de la similitud coseno:', datos.resultados);
+      console.log("Resultados de la similitud coseno:", datos.resultados);
     } catch (error) {
-      console.error('Error al enviar datos al back-end:', error);
+      console.error("Error al enviar datos al back-end:", error);
     }
   };
 
-
-
-
-//debugging -> comprobar que sirve la similitud
+  //debugging -> comprobar que sirve la similitud
 
   const datosUsuario = {
     color_preferido: sliderValue,
@@ -78,25 +69,21 @@ export function QuizPage() {
   };
 
   // Realizar la solicitud al backend
-  fetch('http://localhost:5000/calcular_similitud', {
-    method: 'POST',
+  fetch("http://localhost:5000/calcular_similitud", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ datos_usuario: datosUsuario }),
   })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Respuesta del servidor:', data);
-    // Puedes agregar más lógica aquí según la respuesta del servidor
-  })
-  .catch(error => {
-    console.error('Error en la solicitud:', error);
-  });
-
-
-
-
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Respuesta del servidor:", data);
+      // Puedes agregar más lógica aquí según la respuesta del servidor
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud:", error);
+    });
 
   //resto del front PREGUNTAS
   return (
@@ -236,19 +223,20 @@ export function QuizPage() {
         </div>
         <div className="border-b border-gray-300 my-4 py-6"></div>
 
-        {/* Pregunta 5 */}
+        {/* Pregunta 6 */}
         <h1 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          5. ¿Qué tan importante son los accesorios en tu estilo?
+          5. ¿Qué tan dispuesto/a estás a seguir las tendencias de moda
+          actuales?
         </h1>
         <div className="flex h-full items-center justify-center py-4">
           <img
-            src="./imgs/pregunta5.png"
+            src="./imgs/pregunta6.png"
             alt="Logo"
             className="w-96 h-auto mr-2"
           />
         </div>
         <div className="flex items-center justify-between">
-          <span className="px-4">No importantes</span>{" "}
+          <span className="px-4">Nada dispuesto/a</span>{" "}
           <Slider
             min={1}
             max={5}
@@ -264,53 +252,16 @@ export function QuizPage() {
             step={1}
             dots
           />
-          <span className="px-4">Muy importantes</span>{" "}
-        </div>
-        <div className="border-b border-gray-300 my-4 py-6"></div>
-
-        {/* Pregunta 6 */}
-        <h1 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          6. ¿Qué tan dispuesto/a estás a seguir las tendencias de moda
-          actuales?
-        </h1>
-        <div className="flex h-full items-center justify-center py-4">
-          <img
-            src="./imgs/pregunta6.png"
-            alt="Logo"
-            className="w-96 h-auto mr-2"
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <span className="px-4">Nada dispuesto/a</span>{" "}
-          <Slider
-            min={1}
-            max={5}
-            value={sliderValue6}
-            onChange={handleSliderChange6}
-            marks={{
-              1: "1",
-              2: "2",
-              3: "3",
-              4: "4",
-              5: "5",
-            }}
-            step={1}
-            dots
-          />
           <span className="px-4">Muy dispuesto/a</span>{" "}
         </div>
+        {/* BOTÓN PARA ENVIAR LOS DATOS -> QUIZ */}
+        <button
+          onClick={enviarAlQuiz}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-20"
+        >
+          Enviar el Quiz
+        </button>
       </div>
-
-
-{/* BOTÓN PARA ENVIAR LOS DATOS -> QUIZ */}
-<button
-onClick={enviarAlQuiz}
-className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
->Enviar al Quiz</button>
-
-</div>
-
+    </div>
   );
 }
-
-
